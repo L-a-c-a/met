a00cd95495316dbb9eaa7c1480ab67be9817d31c
Your Name <you@example.com>
2022-07-17 10:23:35 +0200

    inic

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Vis from './sat24/Vis.svelte'
+</script>
+
+<main>
+  <Vis/>
+</main>
+ 
---
+++ src/sat24/Vis.svelte
@@ -0,0 +1,35 @@
+<script>
+  
+  //let fajlnevIdo = 202207150850 //ééééhhnnóópp UTC-ben
+  const datum2fajlnev = (d) => d.toISOString().replaceAll(/[-T:]/g, '').substring(0, 11) + "0"
+  // dátum -> "éééé-hh-nnTóó:pp:....." -> "ééééhhnnóópp....." -> "ééééhhnnóóp0"
+
+  let fajlnevIdo = datum2fajlnev(new Date())
+  
+  const terkepFrissit = () =>
+  {
+    console.log("terkepFrissit" + ': ' + fajlnevIdo)
+  }
+
+  const elozoTerkep = (event) =>
+  {
+    fajlnevIdo-=5
+    if (fajlnevIdo%100 >= 60) fajlnevIdo-=40
+    if (fajlnevIdo/100%100 >= 24) fajlnevIdo-=7600
+  }
+
+  const kovetkezoTerkep = (event) =>
+  {
+    fajlnevIdo+=5
+    if (fajlnevIdo%100 >= 60) fajlnevIdo+=40
+    if (fajlnevIdo/100%100 >= 24) fajlnevIdo+=7600
+  }
+
+</script>
+
+<button on:click="{elozoTerkep}">&lt;</button>
+<input bind:value="{fajlnevIdo}" />
+<button on:click="{kovetkezoTerkep}">&gt;</button>
+<div>
+  <img src="https://hu.sat24.com/image?type=visual&region=hu&timestamp={fajlnevIdo}&anyadkess={new Date().getMilliseconds()}" alt="">
+</div>
