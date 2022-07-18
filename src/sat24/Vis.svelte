e5ed87dddb85752434001ec71f3c5c8314923733
Your Name <you@example.com>
2022-07-18 20:06:47 +0200

    rádiógomb
    és pucolás

--- src/sat24/Vis.svelte
+++ src/sat24/Vis.svelte
@@ -1,44 +1,28 @@
 <script lang="ts">
 import { onMount } from "svelte";
 
-
-  
-  //let fajlnevIdo = 202207150850 //ééééhhnnóópp UTC-ben
-  const datum2fajlnev = (d:Date):string => d.toISOString().replaceAll(/[-T:]/g, '').substring(0, 11) + "0"
-  // dátum -> "éééé-hh-nnTóó:pp:....." -> "ééééhhnnóópp....." -> "ééééhhnnóóp0"
-
-  let fajlnevIdo:number = +datum2fajlnev(new Date())
-  
-  const elozoTerkep0·0 = (event:Event) =>  // 0·0 verzió (· = &middot; &#183; U+00B7   pl. katalán sol·licitada) (pont nem lehet azonosítóban)
-  {
-    fajlnevIdo-=5
-    if (fajlnevIdo%100 >= 60) fajlnevIdo-=40
-    if (fajlnevIdo/100%100 >= 24) fajlnevIdo-=7600
-  }
-
-  const kovetkezoTerkep0·0 = (event:Event) =>
-  {
-    fajlnevIdo+=5
-    if (fajlnevIdo%100 >= 60) fajlnevIdo+=40
-    if (fajlnevIdo/100%100 >= 24) fajlnevIdo+=7600
-  }
+  const datum2fajlnev = (d:Date):string =>
+    { if (d) 
+      {
+        d.setMinutes(Math.trunc(d.getMinutes()/5) * 5, 0, 0)
+        return d.toISOString().replaceAll(/[-T:]/g, '').substring(0, 12)
+      }
+      else return ""
+    }
+    // d.toISOString().replaceAll(/[-T:]/g, '').substring(0, 11) + "0"
+    // dátum -> "éééé-hh-nnTóó:pp:....." -> "ééééhhnnóópp....." -> "ééééhhnnóóp0"
 
   let képtípus:string = "visual"  // visual vagy infraPolair
 
-  //let fajlnevDatum:Date //= new Date()//.setTime(0) az szám
-  //fajlnevDatum.setSeconds(0, 0)
+  let fájlnévDátum:Date
+  
   const fajlnevDatumInic = () =>
   {
     let d:Date = new Date()
     d.setMinutes(Math.trunc(d.getMinutes()/5) * 5, 0, 0)
-    //fajlnevDatum = d
     fájlnévDátum = d
-    // {fajlnevDatum} nem műx (nem frissül), {fájlnévDátum} igen
-    /***/ console.log("dátum inic")
   }
   onMount(fajlnevDatumInic)
-  let fájlnévDátum:Date
-  
 
   const előzőTérkép = (event:Event) =>
   {
@@ -56,18 +40,19 @@ import { onMount } from "svelte";
 
 </script>
 
-<button on:click="{elozoTerkep0·0}">&lt;</button>
-<input bind:value="{fajlnevIdo}" />
-<button on:click="{kovetkezoTerkep0·0}">&gt;</button>
 <div>
-  <img src="https://hu.sat24.com/image?type={képtípus}&region=hu&timestamp={fajlnevIdo}&anyadkess={new Date().getMilliseconds()}" alt="">
+  <input type="radio" id="viz" bind:group={képtípus} name="feny" value="visual" checked>  <label for="viz">Látható</label>
+  <input type="radio" id="inf" bind:group={képtípus} name="feny" value="infraPolair">     <label for="inf">Infravörös</label>
 </div>
 <div>
   <button on:click="{előzőTérkép}">&lt;</button>
-  <input value="{fájlnévDátum?.toISOString().substring(0, 16)}" />
+  <input type="datetime-local" value="{fájlnévDátum?.toISOString().substring(0, 16)}" />  <!-- datetime-local -nak ez a formátum kell. Figyelem: UTC! -->
   <button on:click="{következőTérkép}">&gt;</button>
-  <!----      <button on:click="{()=>console.log(fajlnevDatum)}">{fajlnevDatum} {fajlnevIdo}</button>
-  <!----     {fajlnevDatum?.getMinutes()} 
-  <!----     {fajlnevDatum?.toISOString().substring(0, 16)}
-  <!----> {fájlnévDátum}
 </div>
+<div>
+  <img src="https://hu.sat24.com/image?type={képtípus}&region=hu&timestamp={datum2fajlnev(fájlnévDátum)}&anyadkess={new Date().getMilliseconds()}" alt="">
+</div>
+
+<style>
+  label {display: inline;}  /* public/global.css-ben valamiért block van */
+</style>
